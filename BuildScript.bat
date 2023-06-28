7f4a255315da1db3da9dae99981d7a093795375e
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-06-28 19:14:59 +0530

    Added BuildScript.bat file

---
+++ BuildScript.bat
@@ -0,0 +1,21 @@
+echo %~dp0
+
+SET PATH=%SystemRoot%\System32\WindowsPowerShell\v1.0\;%PATH%
+
+@echo STARTING BUILD NOW
+
+powershell -command " 'dotnet version is' ; dotnet --version; "
+
+dotnet build
+
+dotnet publish -p:PublishProfile=IISProfile
+
+@ECHO DOING LOCAL INSTALLATION NOW
+powershell -command " .\bin\debug\PilaniDevOpBackendCode.deploy.cmd /Y "
+
+@REM PENDING is deployment in
+@REM local macine : C:\tmp\client
+@REM server : development@192.168.x.y
+
+@ECHO DONE BUILD AND DEPLOYMENT
+exit /b 0
