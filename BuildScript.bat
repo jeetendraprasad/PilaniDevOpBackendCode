546c733b9e08e24db5099caa9f9824323e8b663c
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-06-29 13:49:30 +0530

    changes done

--- BuildScript.bat
+++ BuildScript.bat
@@ -8,7 +8,7 @@ powershell -command " 'dotnet version is' ; dotnet --version; "
 
 dotnet build
 
-@REM ssh jeete@192.168.87.1 dir
+ssh jeete@192.168.87.1 "echo Hi From Server"
 
 @REM exit /b 0
 
@@ -16,6 +16,8 @@ dotnet publish -p:PublishProfile=IISProfile
 
 @ECHO DOING LOCAL INSTALLATION NOW
 powershell -command " .\bin\debug\PilaniDevOpBackendCode.deploy.cmd /Y "
+
+scp C:\Users\jeete\Downloads\dotnet-hosting-6.0.19-win.exe jeete@192.168.220.1:/c/tmp
 
 @REM PENDING is deployment in
 @REM local macine : C:\tmp\client
