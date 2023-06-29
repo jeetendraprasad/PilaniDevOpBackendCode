5daafca320b61295d6d1199f1d27f9b31c31af52
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-06-29 22:20:58 +0530

    changes done

--- BuildScript.bat
+++ BuildScript.bat
@@ -1,4 +1,13 @@
+@REM : BUILD SCRIPT AND REMOTE DEPLOYMENT
+@REM : THIS ASSUMES SERVER IP = %SERVER_IP%
+@REM : I HAVE DYNAMIC IP. SO I NEED TO CHANGE THE SERVER EVERY TIME IN THIS SCRIPT
+
 echo %~dp0
+
+SET SERVER_IP=192.168.87.1
+SET SERVER_ADMINUSER=development
+@REM SERVER_FOLDER sould be present on server
+SET SERVER_FOLDER=c:\tmp
 
 SET PATH=%SystemRoot%\System32\WindowsPowerShell\v1.0\;%PATH%
 
@@ -8,7 +17,9 @@ powershell -command " 'dotnet version is' ; dotnet --version; "
 
 dotnet build
 
-ssh jeete@192.168.87.1 "echo Hi From Server"
+ping %SERVER_IP%
+
+ssh %SERVER_ADMINUSER%@%SERVER_IP% "echo Hi From Server"
 
 @REM exit /b 0
 
@@ -17,11 +28,9 @@ dotnet publish -p:PublishProfile=IISProfile
 @ECHO DOING LOCAL INSTALLATION NOW
 powershell -command " .\bin\debug\PilaniDevOpBackendCode.deploy.cmd /Y "
 
-scp C:\Users\jeete\Downloads\dotnet-hosting-6.0.19-win.exe jeete@192.168.87.1:/c/tmp
+scp .\bin\debug %SERVER_ADMINUSER%@%SERVER_IP%:%SERVER_FOLDER%\debug
 
-@REM PENDING is deployment in
-@REM local macine : C:\tmp\client
-@REM server : development@192.168.x.y
+ssh %SERVER_ADMINUSER%@%SERVER_IP% " %SERVER_FOLDER%\debug\PilaniDevOpBackendCode.deploy.cmd /Y "
 
 @ECHO DONE BUILD AND DEPLOYMENT
 exit /b 0
